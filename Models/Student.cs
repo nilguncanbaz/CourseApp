604f41d928f73e81d53dabe05a5aabca1ff602d1
nilgun canbaz <nil.canbaz@hotmail.com>
2019-09-06 14:10:25 +0300

    bootstrap

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,14 +3,14 @@ namespace CourseApp.Models
 {
     public class Student
     {
-        [Required(ErrorMessage="İsminizi Giriniz.")]
+        [Required(ErrorMessage="İsminizi giriniz.")]
         public string Name { get; set; }
-        [Required(ErrorMessage="Eposta adresinizi Giriniz.")]
-        [EmailAddress(ErrorMessage="Epostayı düzgün Giriniz.")]
+        [Required(ErrorMessage="Eposta adresinizi giriniz.")]
+        [EmailAddress(ErrorMessage="Epostayı düzgün giriniz.")]
         public string Email { get; set; }
-        [Required(ErrorMessage="Telefonunuzu Giriniz.")]
+        [Required(ErrorMessage="Telefonunuzu giriniz.")]
         public string Phone { get; set; }
-        [Required(ErrorMessage="Katılım durumunuzu Giriniz.")]
+        [Required(ErrorMessage="Katılım durumunuzu giriniz.")]
         public bool? Confirm { get; set; } // true, false, null
     }
 }
